55e571bec91dd3bdbab6e2e45c7b6a7ee5f32705
Manish Jha <jhas67761@gmail.com>
2024-11-30 19:04:12 +0530

    Create Merge_two_array_without_exta_space.java

---
+++ Merge_two_array_without_exta_space.java
@@ -0,0 +1,17 @@
+public class Main{
+  static void merge(int[]  a , int[]  b) { 
+  int i = a.length-1 , j = 0;
+    while(i >= 0 && j < b.length) {
+      if(a[i > b[j{
+         int temp = arr[i] ;
+         arr[i] = arr[j] ;
+           arr[j] = temp ;   //  b array ko maximum banana hai in compare to a array 
+  else{
+    i -- ;
+    j++; }
+  }
+    Arrays.sort(a) ;
+    Arrays.sort(b) ;
+  }
+}
+  
